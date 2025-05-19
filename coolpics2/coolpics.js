6af7abb279e9dffcde98b859fa1e69070353caca
YesterFence <ryantatsuyalee@gmail.com>
2025-05-18 23:19:12 -0600

    Trying

--- coolpics2/coolpics.js
+++ coolpics2/coolpics.js
@@ -1,63 +1,71 @@
-// const button = document.getElementById('.menuButton');
-const button = document.querySelector('.menuButton');
-const link = document.querySelector('.links');
-const image = document.querySelectorAll('.pictures img');
+// Menu Management
+const button = document.querySelector('#menuToggle');
+const theLinks = document.querySelector('#headLinks');
+// Picture Management
+const image = document.querySelectorAll('.pictures');
 const enlarge = document.querySelector('.enhance');
 const enlargeImg = document.querySelector('.enhance-img');
 const close = document.querySelector('.close-enhance');
 
-button.addEventListener('click', function ()
-{
-    link.classList.toggle('hide');
-});
 
-enlarge.addEventListener('click', function(event)
+button.addEventListener('click', () =>
 {
-    if (event.target === enlarge)
-    {
-        enlarge.classList.add('hide');
-    }
-});
-// Needed !!!!
-const modal = document.createElement('dialog');
-modal.innerHTML=`<img><button class='close-viwer'>X</button>`;
-
-close.addEventListener('click', function()
-{
-    enlargeImg.src = 'coolpics2/pics/norris-full.jpeg';
-    enlarge.classList.add('hide');
-});
-// Do NOT USE FOR EACH
-image.forEach(function(img)
-{
-    img.addEventListener('click', function()
-    {
-        enlargeImg.src = img.src;
-        enlarge.classList.remove('hide');
-    });
+    theLinks.classList.toggle('hide');
 });
 
 function handleResize()
 {
-    if (window.innerWidth > 1000)
+    if (window.innerWidth > 1200)
     {
-        link.classList.remove("hide");
+        theLinks.classList.remove("hide");
     }
     else
     {
-        link.classList.add("hide");
+        theLinks.classList.add("hide");
     }
+
 }
-
-
-let btn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu')
-btn.addEventListener('click', toggleMenu);
-function toggleMenu()
-{
-    menubar.classList.toggle('hide');
-}
-
-
+// button.addEventListener('click', toggleMenu);
 handleResize();
 window.addEventListener("resize", handleResize);
+// image.addEventListener('click', function()
+// {
+//     enlargeImg.src = "pics/norris-full.jpeg";
+//     enlarge.classList.remove('hide');
+// });
+
+// close.addEventListener('click', function()
+// {
+//     enlarge.classList.add('hide');
+//     enlargeImg.src = '';
+// });
+
+
+
+
+// enlarge.addEventListener('click', function(event)
+// {
+//     if (event.target === enlarge)
+//     {
+//         enlarge.classList.add('hide');
+//     }
+// });
+
+// // Needed !!!!
+// const modal = document.createElement('dialog');
+// modal.innerHTML=`<img><button class='close-viwer'>X</button>`;
+
+// close.addEventListener('click', function()
+// {
+//     enlargeImg.src = 'coolpics2/pics/norris-full.jpeg';
+//     enlarge.classList.add('hide');
+// });
+
+
+// let btn = document.querySelector('.menu-btn');
+// const menu = document.querySelector('.menu')
+// btn.addEventListener('click', toggleMenu);
+// function toggleMenu()
+// {
+//     menubar.classList.toggle('hide');
+// }
