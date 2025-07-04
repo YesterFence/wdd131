3cc665f09b3c3f4bcbba1047fb63adec845e0868
Ryan <ryantatsuyalee@gmail.com>
2025-07-04 10:41:10 -0700

    Wk11 Site Plan

---
+++ aCheesyProject/cheese.js
@@ -0,0 +1,93 @@
+const cheeses = [
+    {
+        name: "American",
+        description: "",
+        img: "aCheesyProject/Images/rsz_american.png",
+        price: 3.59,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Gouda",
+        description: "",
+        img: "aCheesyProject/Images/rsz_gouda.jpg",
+        price: 6.79,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Brie",
+        description: "",
+        img: "aCheesyProject/Images/rsz_brie.png",
+        price: 4.45,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Cheddar",
+        description: "",
+        img: "aCheesyProject/Images/rsz_cheddar.jpg",
+        price: 2.94,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Colby Jack",
+        description: "",
+        img: "aCheesyProject/Images/rsz_colbyjack.jpg",
+        price: 3.50,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Ricotta",
+        description: "",
+        img: "aCheesyProject/Images/rsz_ricotta.png",
+        price: 3.93,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Pepper Jack",
+        description: "",
+        img: "aCheesyProject/Images/rsz_pepperjack.jpg",
+        price: 3.74,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Parmesan",
+        description: "",
+        img: "aCheesyProject/Images/rsz_parmesan.png",
+        price: 4.95,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Mozzarella",
+        description: "",
+        img: "aCheesyProject/Images/rsz_mozzarella.jpg",
+        price: 4.95,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Swiss",
+        description: "",
+        img: "aCheesyProject/Images/rsz_swiss.png",
+        price: 2.99,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Gorgonzola",
+        description: "",
+        img: "aCheesyProject/Images/rsz_gorgonzola.png",
+        price: 4.99,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Bleu",
+        description: "",
+        img: "aCheesyProject/Images/rsz_bleu.jpg",
+        price: 7.06,
+        storeTag: ["", "", ""]
+    },
+    {
+        name: "Unprocessed",
+        description: "",
+        img: "aCheesyProject/Images/unprocessed.jpeg",
+        price: 3.75,
+        storeTag: ["", "", ""]
+    }
+]
